2324ef47c6d2e0c2519c02b96ac4bb2c7ffcf6d5
Gilbert-2 <gilbertrick81@gmail.com>
2024-09-25 10:43:57 +0200

    added validation

--- src/main/java/com/example/demo/Model/MyAppUser.java
+++ src/main/java/com/example/demo/Model/MyAppUser.java
@@ -4,6 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Transient;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
@@ -23,6 +24,7 @@ public class MyAppUser {
     @NotBlank(message = "Password is required")
     @Size(min = 8, message = "Password should be at least 8 characters long")
     private String password;
+    @Transient
     @NotBlank(message = "Password confirmation is required")
     private String passwordConfirmation;
     public Long getId() {
